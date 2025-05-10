d86cc56aabf0e86841e5081679f7d28015d4b762
Igor Savin <costonied@gmail.com>
2025-05-10 15:18:24 +0300

    feat(header): добавить первую версию компонента "Заголовок"

---
+++ code/05 Essentials Practice/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header id="header">
+            <img src="investment-calculator-logo.png" alt="Big bag with money" />
+            <h1>Investment Calculator</h1>
+        </header>
+    )
+}
+
+export default Header
