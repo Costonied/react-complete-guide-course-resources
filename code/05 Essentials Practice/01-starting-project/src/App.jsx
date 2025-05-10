74b31c8f267b3f1c7a382dbd8ad33bcc5b710dc2
Igor Savin <costonied@gmail.com>
2025-05-10 17:51:04 +0300

    feat(calculator): добавить структуру для хранения введенных пользовательских данных

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,13 +1,32 @@
+import {useState} from "react";
+
 import Header from "./components/Header.jsx";
 import Calculator from "./components/Calculator.jsx";
 import Board from "./components/Board.jsx";
 
 function App() {
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue,
+            };
+        });
+    }
+
     return (
         <>
             <Header/>
             <main>
-                <Calculator />
+                <Calculator userInput={userInput} onChange={handleChange}/>
                 <Board />
             </main>
         </>
--- code/05 Essentials Practice/01-starting-project/src/components/Calculator.jsx
+++ code/05 Essentials Practice/01-starting-project/src/components/Calculator.jsx
@@ -1,28 +1,42 @@
-function Calculator() {
-    return (
-        <section id="user-input">
+function Calculator({userInput, onChange}) {
+    return (<section id="user-input">
             <div className="input-group">
                 <p>
                     <label htmlFor={"inputInitialInvestment"}>Initial Investment</label>
-                    <input id={"inputInitialInvestment"} type={"number"} required={true} />
+                    <input
+                        id={"inputInitialInvestment"} type={"number"} required={true}
+                        value={userInput.initialInvestment}
+                        onChange={(event) => onChange('initialInvestment', event.target.value)}
+                    />
                 </p>
                 <p>
                     <label htmlFor={"inputAnnualInvestment"}>Annual Investment</label>
-                    <input id={"inputAnnualInvestment"} type={"number"} required={true} />
+                    <input
+                        id={"inputAnnualInvestment"} type={"number"} required={true}
+                        value={userInput.annualInvestment}
+                        onChange={(event) => onChange('annualInvestment', event.target.value)}
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label htmlFor={"inputExpectedReturn"}>Expected Return</label>
-                    <input id={"inputExpectedReturn"} type={"number"} required={true} />
+                    <input
+                        id={"inputExpectedReturn"} type={"number"} required={true}
+                        value={userInput.expectedReturn}
+                        onChange={(event) => onChange('expectedReturn', event.target.value)}
+                    />
                 </p>
                 <p>
                     <label htmlFor={"inputDuration"}>Duration</label>
-                    <input id={"inputDuration"} type={"number"} required={true} />
+                    <input
+                        id={"inputDuration"} type={"number"} required={true}
+                        value={userInput.duration}
+                        onChange={(event) => onChange('duration', event.target.value)}
+                    />
                 </p>
             </div>
-        </section>
-    )
+        </section>)
 }
 
 export default Calculator
