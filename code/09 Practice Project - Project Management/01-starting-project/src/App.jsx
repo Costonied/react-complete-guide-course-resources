a2fb91fdcbf78086b043e4045ccb9dbbf02accea
Igor Savin <costonied@gmail.com>
2025-05-23 15:25:03 +0300

    feat(component): логика открытия формы создания нового проекта

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,12 +1,35 @@
+import {useState} from "react";
+
 import MenuProjects from "./components/MenuProjects.jsx"
 import ProjectNoSelected from "./components/ProjectNoSelected.jsx"
+import ProjectCreateForm from "./components/ProjectCreateForm.jsx"
 
 function App() {
+
+    const projects = []
+
+    const [selectedProject, setSelectedProject] = useState(null);
+    const [isProjectCreating, setIsProjectCreating] = useState(false);
+
+    function handleCreateNewProjectClicked() {
+        setSelectedProject(null);
+        setIsProjectCreating(true);
+    }
+
+    function handleGoHomeClicked() {
+        setSelectedProject(null);
+        setIsProjectCreating(false);
+    }
+
     return (
         <div className="flex h-screen">
-            <MenuProjects/>
+            <MenuProjects
+                onAddProjectClicked={handleCreateNewProjectClicked}
+                onTitleClicked={handleGoHomeClicked}/>
             <div className="w-4/5 p-4 mt-10">
-                <ProjectNoSelected />
+                {selectedProject === null && !isProjectCreating &&
+                    <ProjectNoSelected onCreateNewProjectClicked={handleCreateNewProjectClicked}/>}
+                {isProjectCreating && <ProjectCreateForm/>}
             </div>
         </div>
     );
--- code/09 Practice Project - Project Management/01-starting-project/src/components/MenuProjects.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/MenuProjects.jsx
@@ -1,8 +1,10 @@
-export default function MenuProjects() {
+export default function MenuProjects({ onAddProjectClicked, onTitleClicked }) {
     return (
         <div className="w-1/5 bg-gray-900 text-white pl-8 pt-10 mt-10 rounded-tr-3xl">
-            <h2 className="text-2xl font-bold mb-4">YOUR PROJECTS</h2>
-            <button className="bg-gray-700 hover:bg-gray-600 text-gray-200 font-bold py-2 px-4 rounded mb-4">
+            <h2 onClick={onTitleClicked} className="text-2xl font-bold mb-4">YOUR PROJECTS</h2>
+            <button
+                onClick={onAddProjectClicked}
+                className="bg-gray-700 hover:bg-gray-600 text-gray-200 font-bold py-2 px-4 rounded mb-4">
                 + Add Project
             </button>
             <div className="vertical-menu">
